e2b7a34c65e13cc8ee733edacdf4155f9ed7a905
Declan Zhang <deqzhang@ucdavis.edu>
2017-11-06 23:47:06 -0800

    quicksort,insertionsort,selectionsort

--- README.md
+++ README.md
@@ -1,21 +1,56 @@
-# Rust and Design Pattern for Data Structure 
-- Currently the abstract sorting function is implemented.
+# Rust and Design Pattern for Data Structure
+
+- Sorter
+- List
 
 ### Template Method Pattern
+
 - Used when a system must implement an alogrithm
 - with an invariant part, which always executes the same way
 - but which depends on variant parts that are implemented differently depends on the circumstance
+- The pattern consists of two parts:
+  - template function: the function that always execute the same way (invariant), e.g sort
+  - primitive function: the function is varied based on circumstance, e.g split & join
 
-#### Templates for Comparision based Sorting Algorithm
+#### Templates for Comparision based Sorting Algorithms
+
 - Merritt Sort Taxonomy  
 
 - ```
         split
-        /   \
-   sort L1, sort L2
-        |    |
-   sorted L1', sorted L2'
+       /     \
+      L1      L2
+      |       |
+    sort     sort
+      |       |
+     L1'      L2'
         \    /
          join
    ```
 
+- ```python
+   sort(a,lo,hi):
+     if lo < hi: #at least two elements
+       split(a,lo,mid,hi) #postcondition: low < mid <= high
+       sort(a,lo,mid-1)
+       sort(a,mid,hi)
+       join(a,low,hi)
+   ```
+
+- run time analysis
+
+   - T(n) refers to total time for a recursive function, e.g sort
+   - O(n) refers to running time for a non-recursive function e.g split,join
+
+## Composite, State, Visitor Pattern
+
+#### List
+
+- Classic List
+  - List & Node
+  - single linked list
+  - single linked list with tail pointer, or a dummy head
+  - double linked list
+  - double linked list with a dummy head
+- Lists that adopted Composite,State,Vistor Design Pattern
+  - ​
--- src/hi.rs
+++ src/hi.rs
@@ -1,61 +1,149 @@
-trait Sorter<T:PartialOrd+Clone> {
-   fn sort(&self, ar: &mut Vec<T>, low: usize, high: usize) {
-      if high < ar.len() && low < high {
-         let mut mid = 0;
-         self.split(ar, low, &mut mid, high);
-         self.sort(ar, low, mid-1);
-         self.sort(ar, mid, high);
-         self.join(ar, low, mid, high);
-      }
-   }
-   fn split(&self, ar: &mut Vec<T>, low: usize, mid: &mut usize, high: usize);
-   fn join(&self, ar: &mut Vec<T>, low: usize, mid: usize, high: usize);
-}
-struct MergeSorter;
-
-impl<T: PartialOrd+Clone> Sorter<T> for MergeSorter {
-   fn split(&self, ar: &mut Vec<T>, low: usize, mid: &mut usize, high: usize) {
-      let _ = ar;
-      *mid = (high - low + 1) / 2 + low;
-   }
-   fn join(&self, ar: &mut Vec<T>, low: usize, mid: usize, high: usize) {
-      
-      let (mut i, mut j) = (low, mid);
-      let mut vec = Vec::new();
-      for _ in low..high + 1 {
-         if i >= mid {
-            vec.push(ar[j].clone());
-            j += 1;
-         } else if j > high {
-            vec.push(ar[i].clone());
-            i += 1;
-         } else {
-            if ar[i] > ar[j] {
-               vec.push(ar[j].clone());
-               j += 1;
-            } else {
-               vec.push(ar[i].clone());
-               i += 1;
-            }
+/** 
+ * visibility control
+   mod my_module {
+      pub struct MergeSorter;
+      struct MergeSort;
+      impl Trait for MergeSort { ... }
+      impl MergeSorter { 
+         pub fn sort(&self){
+            MergeSort.sort()
          }
       }
-      for idx in low..high + 1 {
-         ar[idx] = vec[idx-low].clone();
-      }
-   }
+   } 
+*/
+struct MergeSorter;
+struct QuickSorter;
+struct SelectionSorter;
+struct InsertionSorter;
+trait Sorter<T: PartialOrd + Clone> {
+    fn sort(&self, ar: &mut Vec<T>, low: usize, high: usize) {
+        if high >= ar.len() {
+            return;
+        }
+        if low < high {
+            let mut mid = 0;
+            //pre-condition 0<low<high<cap
+            //post-condition low < mid <= high
+            self.split(ar, low, &mut mid, high);
+            self.sort(ar, low, mid - 1);
+            self.sort(ar, mid, high);
+            self.join(ar, low, mid, high);
+        }
+    }
+    fn split(&self, ar: &mut Vec<T>, low: usize, mid: &mut usize, high: usize);
+    fn join(&self, ar: &mut Vec<T>, low: usize, mid: usize, high: usize);
 }
+
+impl<T> Sorter<T> for MergeSorter
+where
+    T: PartialOrd + Clone,
+{
+    fn split(&self, ar: &mut Vec<T>, low: usize, mid: &mut usize, high: usize) {
+        let _ = ar;
+        *mid = (high - low + 1) / 2 + low;
+    }
+    fn join(&self, ar: &mut Vec<T>, low: usize, mid: usize, high: usize) {
+        let (mut i, mut j) = (low, mid);
+        let mut vec = Vec::new();
+        // vec.resize(high-low+1,Default::default());
+        ////? Optimization: how to create a vector<T> and size is len
+        ////? so we no longer to create tempory vectors.
+        for _ in low..high + 1 {
+            if i >= mid {
+                vec.push(ar[j].clone());
+                j += 1;
+            } else if j > high {
+                vec.push(ar[i].clone());
+                i += 1;
+            } else {
+                if ar[i] > ar[j] {
+                    vec.push(ar[j].clone());
+                    j += 1;
+                } else {
+                    vec.push(ar[i].clone());
+                    i += 1;
+                }
+            }
+        }
+        for idx in low..high + 1 {
+            ar[idx] = vec[idx - low].clone();
+        }
+    }
+}
+impl<T> Sorter<T> for QuickSorter
+where
+    T: PartialOrd + Clone,
+{
+    fn split(&self, ar: &mut Vec<T>, low: usize, mid: &mut usize, high: usize) {
+      let key = ar[high].clone();
+        *mid = low;
+        for j in low..high + 1 {
+            if ar[j] <= key {
+                ar.swap(j, *mid);
+                *mid += 1;
+            }
+        }
+        *mid = if *mid > high { high } else { *mid };
+    }
+    fn join(&self, _ar: &mut Vec<T>, _low: usize, _mid: usize, _high: usize) {}
+}
+impl<T: PartialOrd + Clone> Sorter<T> for SelectionSorter {
+    fn split(&self, ar: &mut Vec<T>, low: usize, mid: &mut usize, high: usize) {
+        let mut index_of_max = low;
+        for i in low + 1..high + 1 {
+            if ar[i] > ar[index_of_max] {
+                index_of_max = i;
+            }
+        }
+        ar.swap(index_of_max, high);
+        *mid = high;
+    }
+    fn join(&self, _ar: &mut Vec<T>, _low: usize, _mid: usize, _high: usize) {}
+}
+impl<T: PartialOrd + Clone> Sorter<T> for InsertionSorter {
+    fn split(&self, _ar: &mut Vec<T>, _low: usize, mid: &mut usize, high: usize) {
+        *mid = high;
+    }
+    fn join(&self, ar: &mut Vec<T>, _low: usize, mid: usize, _high: usize) {
+        //precondition mid = high && ar[low..high] is sorted
+        let key = ar[mid].clone();
+        let mut j = mid - 1;
+        while key < ar[j] {
+            ar[j+1] = ar[j].clone();
+            if j == 0 { /* On a case where key is unique minimum*/
+                ar[j] = key;
+                return;
+            }
+            j -= 1;
+            // j = j.wrapping_sub(1);
+        }
+        ar[j+1] = key;
+    }
+}
+
+
 fn main() {
-   let i = 5;
-   for n in 1..i {
-      println!("{}", n);
-   }
-
-   let mut a = vec!(12,13,14,15,4,3,2,1,4,5,6,9,8,7);
-
-   let len = a.len();
-   MergeSorter.sort(&mut a,0,len-1);
-
-   println!("Sorted:{:?}", a);
-   println!("{:05b}", 1);
-   println!("{}", 4 / 3);
+    let i = 5;
+    for n in 1..i {
+        println!("{}", n);
+    }
+    let mut a = vec![12, 13, 14, 15, 4, 3, 2, 1, 4, 5, 6, 9, 8, 7];
+    let mut b = a.clone();
+    let mut c = a.clone();
+    let mut d = a.clone();
+    //let len  = a.len(); // optimize merge sort with size len vector
+    let high = a.len() - 1;
+    MergeSorter.sort(&mut a, 0, high);
+    QuickSorter.sort(&mut b, 0, high);
+    SelectionSorter.sort(&mut c, 0, high);
+    InsertionSorter.sort(&mut d, 0, high);
+    println!("MergeSorter:     {:?}", a);
+    println!("QuickSorter:     {:?}", b);
+    println!("SelectionSorter: {:?}", c);
+    println!("InsertionSorter: {:?}", d);
+    println!("{:05b}", 1.clone());
+    for i in (1..10+1).rev(){
+        println!("{:?}", i);
+    }
+    println!("{}", 4 / 3);
 }
